975543e5e7788f67ea7ed24e5ef35d5dbb142f97
Chronister <Sara.Chronister@doh.wa.gov>
2025-08-05 17:47:30 -0700

    query_dd_varname_crosswalk documentation

---
+++ R/query_dd_varname_crosswalk.R
@@ -0,0 +1,12 @@
+#' Query API and Data Details API Crosswalk R Object
+#'
+#' A crosswalk of the API fields used to query a field and their corresponding data details fields
+#'
+#' @format ## `query_dd_varname_crosswalk`
+#' A data frame with 36 rows and 2 columns:
+#' \describe{
+#'  \item{QueryAPI}{data.frame of the ESSENCE tab}
+#'  \item{DataDetailsAPI}{List with length 3:}
+#' }
+#' @source <https://www.cdc.gov/nssp/biosense/docs/NSSP-Data-Dictionary-508.xlsx>
+"query_dd_varname_crosswalk"
